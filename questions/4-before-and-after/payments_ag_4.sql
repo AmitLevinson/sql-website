e9de1396b7277afa9b5a5f8f82737a2e0d1f1e47
Amit Grinson <amitlevinson1@gmail.com>
2023-07-19 21:33:27 +0300

    add q 2 data

---
+++ questions/4-before-and-after/payments_ag_4.sql
@@ -0,0 +1,25 @@
+create table payments_ag_4 (
+	user_id INT,
+	payment_date DATE,
+	amount DECIMAL(4,1)
+);
+insert into payments_ag_4 (user_id, payment_date, amount) values (4, '11/26/2022', 89.7);
+insert into payments_ag_4 (user_id, payment_date, amount) values (5, '11/29/2022', 57.5);
+insert into payments_ag_4 (user_id, payment_date, amount) values (4, '10/14/2022', 52.6);
+insert into payments_ag_4 (user_id, payment_date, amount) values (5, '12/3/2022', 18.3);
+insert into payments_ag_4 (user_id, payment_date, amount) values (2, '10/14/2022', 73.9);
+insert into payments_ag_4 (user_id, payment_date, amount) values (5, '11/30/2022', 4.2);
+insert into payments_ag_4 (user_id, payment_date, amount) values (1, '11/13/2022', 4.0);
+insert into payments_ag_4 (user_id, payment_date, amount) values (2, '11/9/2022', 97.3);
+insert into payments_ag_4 (user_id, payment_date, amount) values (3, '10/22/2022', 4.6);
+insert into payments_ag_4 (user_id, payment_date, amount) values (4, '10/16/2022', 42.0);
+insert into payments_ag_4 (user_id, payment_date, amount) values (4, '12/6/2022', 89.6);
+insert into payments_ag_4 (user_id, payment_date, amount) values (5, '11/29/2022', 36.9);
+insert into payments_ag_4 (user_id, payment_date, amount) values (5, '10/19/2022', 62.5);
+insert into payments_ag_4 (user_id, payment_date, amount) values (1, '11/10/2022', 61.8);
+insert into payments_ag_4 (user_id, payment_date, amount) values (2, '10/31/2022', 17.5);
+insert into payments_ag_4 (user_id, payment_date, amount) values (2, '11/10/2022', 5.7);
+insert into payments_ag_4 (user_id, payment_date, amount) values (1, '10/13/2022', 71.7);
+insert into payments_ag_4 (user_id, payment_date, amount) values (2, '11/2/2022', 35.8);
+insert into payments_ag_4 (user_id, payment_date, amount) values (4, '11/21/2022', 52.3);
+insert into payments_ag_4 (user_id, payment_date, amount) values (2, '10/19/2022', 31.2);
