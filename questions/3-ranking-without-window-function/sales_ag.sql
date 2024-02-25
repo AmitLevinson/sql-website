31c6fd585bbf0a040f08958a17481d762d054afe
Amit Grinson <amitlevinson1@gmail.com>
2024-02-25 21:38:12 +0200

    readd q3sql

---
+++ questions/3-ranking-without-window-function/sales_ag.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE sales_ag (
+	year INT,
+	category varchar(15),
+	amount INT
+);
+
+
+INSERT into sales_ag (year, category, amount) values (2000, 'Automotive', 89);
+INSERT into sales_ag (year, category, amount) values (2000, 'Computers', 59);
+INSERT into sales_ag (year, category, amount) values (2000, 'Books', 19);
+INSERT into sales_ag (year, category, amount) values (2000, 'Movies', 74);
+INSERT into sales_ag (year, category, amount) values (2000, 'Garden', 74);
+INSERT into sales_ag (year, category, amount) values (2001, 'Clothing', 49);
+INSERT into sales_ag (year, category, amount) values (2001, 'Computers', 53);
+INSERT into sales_ag (year, category, amount) values (2001, 'Automotive', 65);
+INSERT into sales_ag (year, category, amount) values (2001, 'Garden', 24);
+INSERT into sales_ag (year, category, amount) values (2001, 'Movies', 39);
